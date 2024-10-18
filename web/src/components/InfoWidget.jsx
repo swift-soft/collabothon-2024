334ae3afa289e2191f21be604bfb95d015b1906e
bnowacki <bknowacki@gmail.com>
2024-10-18 14:46:34 +0200

    top widgets

--- web/src/components/InfoWidget.jsx
+++ web/src/components/InfoWidget.jsx
@@ -1,19 +1,18 @@
 import { Stack, Heading, Button, Spacer } from "@chakra-ui/react";
-import { IconMail } from "@tabler/icons-react";
 import { ArrowForwardIcon } from "@chakra-ui/icons";
 
-export default function InfoWidget() {
+export default function InfoWidget({ title, link, icon }) {
   return (
     <Stack alignItems="left" spacing={4} h="100%" p={4}>
-      <IconMail size={48} />
-      <Heading fontSize="2xl">10 Messages</Heading>
+      {icon}
+      <Heading fontSize="2xl">{title}</Heading>
       <Spacer />
       <Button
         variant="link"
         width="max-content"
         rightIcon={<ArrowForwardIcon />}
       >
-        Open mailbox
+        {link}
       </Button>
     </Stack>
   );
--- web/src/components/TopWidgets.jsx
+++ web/src/components/TopWidgets.jsx
@@ -1,3 +1,4 @@
+import { IconMail, IconPig, IconReceipt2 } from "@tabler/icons-react";
 import InfoWidget from "./InfoWidget";
 import Widget from "./Widget";
 
@@ -5,10 +6,26 @@ export default function TopWidgets() {
   return (
     <>
       <Widget colSpan={2}>
-        <InfoWidget />
+        <InfoWidget
+          title="10 Messages"
+          link="Open mailbox"
+          icon={<IconMail size={48} />}
+        />
       </Widget>
-      <Widget colSpan={2} />
-      <Widget colSpan={2} />
+      <Widget colSpan={2}>
+        <InfoWidget
+          title="3 new orders to be released"
+          link="Release now"
+          icon={<IconReceipt2 size={48} />}
+        />
+      </Widget>
+      <Widget colSpan={2}>
+        <InfoWidget
+          title="12 new orders to be released"
+          link="Release now"
+          icon={<IconPig size={48} />}
+        />
+      </Widget>
     </>
   );
 }
