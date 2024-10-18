22ca1bd75e5031cc884141a4a9c37ab434612148
bnowacki <bknowacki@gmail.com>
2024-10-18 20:14:36 +0200

    top widgets

--- web/src/components/InfoWidget.jsx
+++ web/src/components/InfoWidget.jsx
@@ -1,14 +1,52 @@
-import { Stack, Heading, Button, Spacer, Flex } from "@chakra-ui/react";
+import {
+  Stack,
+  Heading,
+  Button,
+  Spacer,
+  Flex,
+  Box,
+  Text,
+} from "@chakra-ui/react";
 import { ArrowForwardIcon } from "@chakra-ui/icons";
 
-export default function InfoWidget({ title, link, icon }) {
+export default function InfoWidget({ title, link, icon, items }) {
   return (
     <Stack alignItems="left" spacing={4} h="100%" p={4}>
       <Flex align="center" gap={4}>
         {icon}
         <Heading fontSize="xl">{title}</Heading>
       </Flex>
-      <Spacer />
+      <Stack spacing={2}>
+        {items?.map((item, i) => (
+          <Flex
+            key={i}
+            alignItems="center"
+            gap={2}
+            boxShadow="md"
+            rounded="lg"
+            p={2}
+            cursor="pointer"
+            transition="all 250ms ease"
+            bg="whiteAlpha.600"
+            _hover={{ bg: "whiteAlpha.900" }}
+          >
+            <Box
+              rounded="full"
+              border="1px solid"
+              borderColor="brand.navy.500"
+              aspectRatio={1}
+              p={2}
+            >
+              {item.icon}
+            </Box>
+            <Stack spacing={0}>
+              <Text>{item.text}</Text>
+              <Text>{item.date}</Text>
+            </Stack>
+          </Flex>
+        ))}
+      </Stack>
+
       <Button
         variant="link"
         width="max-content"
--- web/src/components/TopWidgets.jsx
+++ web/src/components/TopWidgets.jsx
@@ -1,8 +1,42 @@
-import { IconCheckbox, IconMail } from "@tabler/icons-react";
+import {
+  IconAlertCircle,
+  IconArrowBack,
+  IconCheckbox,
+  IconMail,
+  IconMessage,
+  IconTransfer,
+  IconUserExclamation,
+} from "@tabler/icons-react";
 import InfoWidget from "./InfoWidget";
 import Widget from "./Widget";
 import { Flex } from "@chakra-ui/react";
 import ConsultantsWidget from "./ConsultantsWidget";
+
+const messages = [
+  {
+    icon: <IconMessage />,
+    text: "Lorem ipsum dolor sit...",
+    date: "2024-10-16",
+  },
+  {
+    icon: <IconAlertCircle />,
+    text: "Lorem ipsum dolor sit...",
+    date: "2024-10-16",
+  },
+];
+
+const requests = [
+  {
+    icon: <IconTransfer />,
+    text: "Transfer request",
+    date: "2024-10-16",
+  },
+  {
+    icon: <IconArrowBack />,
+    text: "Refund request",
+    date: "2024-10-16",
+  },
+];
 
 export default function TopWidgets() {
   return (
@@ -12,6 +46,7 @@ export default function TopWidgets() {
           title="7 New messages"
           link="Open mailbox"
           icon={<IconMail size={36} />}
+          items={messages}
         />
       </Widget>
       <Widget colSpan={2} rowSpan={2} flex={1}>
@@ -19,6 +54,7 @@ export default function TopWidgets() {
           title="3 Actions to approve"
           link="Go to approval requests"
           icon={<IconCheckbox size={36} />}
+          items={requests}
         />
       </Widget>
       <Widget colSpan={2} rowSpan={2} flex={1}>
