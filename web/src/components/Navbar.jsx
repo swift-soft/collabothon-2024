d8db012999fe43dd59432e6fa4bdc12dfb1ae125
Stanisław Marciniak <marciniak.dev@gmail.com>
2024-10-18 14:21:31 +0200

    navbar

--- web/src/components/Navbar.jsx
+++ web/src/components/Navbar.jsx
@@ -1,6 +1,14 @@
-import { Box, Container, Img, Flex, Text, Button } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Img,
+  Flex,
+  Text,
+  Button,
+  VStack,
+} from "@chakra-ui/react";
 
-const links = [
+const linksDown = [
   "Accounts & Cash Management",
   "Financing & Foreign Trade",
   "Investment & Risk Management",
@@ -8,28 +16,56 @@ const links = [
   "Help & Contact",
 ];
 
+const linksUp = [
+  "Private Clients",
+  "Business Clients",
+  "Wealth Management",
+  "Corporate Clients",
+  "Careers",
+];
+
 export default function Navbar() {
   return (
     <Box bg="brand.navy.500" py={4}>
-      <Container maxW="container.xl">
-        <Flex gap={4} align="center">
-          <Img src="/imgs/logo.svg" />
-          {links.map((l) => (
-            <Text
-              key={l}
-              color="white"
-              opacity="0.8"
-              fontSize="16px"
-              cursor="pointer"
-              _hover={{ opacity: 1 }}
-            >
-              {l}
-            </Text>
-          ))}
+      <Container maxW="90%">
+        <Flex justify="space-between">
+          <Flex gap={16}>
+            <Img src="/public/imgs/logo.svg" />
+            <VStack gap={10}>
+              <Flex gap={4} width="full">
+                {linksUp.map((l) => (
+                  <Text
+                    key={l}
+                    color="white"
+                    opacity="0.9"
+                    fontSize="18px"
+                    cursor="pointer"
+                    _hover={{ opacity: 1 }}
+                  >
+                    {l}
+                  </Text>
+                ))}
+              </Flex>
+              <Flex gap={4} align="center">
+                {linksDown.map((l) => (
+                  <Text
+                    key={l}
+                    color="white"
+                    opacity="0.8"
+                    fontSize="16px"
+                    cursor="pointer"
+                    _hover={{ opacity: 1 }}
+                  >
+                    {l}
+                  </Text>
+                ))}
+              </Flex>
+            </VStack>
+          </Flex>
           <Button variant="ghost" color="white" colorScheme="whiteAlpha">
             <Flex align="center" gap={2}>
-              <Img src="/imgs/lock.svg" />
-              Log out
+              <Img src="/public/imgs/lock.svg" />
+              Login
             </Flex>
           </Button>
         </Flex>
