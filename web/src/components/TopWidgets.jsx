decd990942964fea5b2c0527120fbf4e423e6b70
Stanisław Marciniak <marciniak.dev@gmail.com>
2024-10-18 17:28:57 +0200

    row update

--- web/src/components/TopWidgets.jsx
+++ web/src/components/TopWidgets.jsx
@@ -5,21 +5,21 @@ import Widget from "./Widget";
 export default function TopWidgets() {
   return (
     <>
-      <Widget colSpan={2} rowSpan={1}>
+      <Widget colSpan={2} rowSpan={2}>
         <InfoWidget
           title="10 Messages"
           link="Open mailbox"
           icon={<IconMail size={48} />}
         />
       </Widget>
-      <Widget colSpan={2} rowSpan={1}>
+      <Widget colSpan={2} rowSpan={2}>
         <InfoWidget
           title="3 new orders to be released"
           link="Release now"
           icon={<IconReceipt2 size={48} />}
         />
       </Widget>
-      <Widget colSpan={2} rowSpan={1}>
+      <Widget colSpan={2} rowSpan={2}>
         <InfoWidget
           title="12 new orders to be released"
           link="Release now"
